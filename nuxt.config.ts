2f2088566ced8f2c01f608bdef186f3862af6f95
corn <dalcon10280@gmail.com>
2024-09-28 11:00:03 +0900

    feat: add app page toolbar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@vueuse/nuxt", "@nuxt/icon"]
+  modules: ["@nuxt/ui", "@vueuse/nuxt", "@nuxt/icon", '@nuxt/eslint'],
+
+  ui: {
+    safelistColors: ['primary', 'red', 'orange', 'green']
+  },
+
+  colorMode: {
+    disableTransition: true
+  },
+
+  routeRules: {
+    // Temporary workaround for prerender regression. see https://github.com/nuxt/nuxt/issues/27490
+    '/': { prerender: true }
+  },
+
+  buildModules: [
+    '@nuxtjs/date-fns',
+  ],
+
+  compatibilityDate: "2024-09-28"
 })
