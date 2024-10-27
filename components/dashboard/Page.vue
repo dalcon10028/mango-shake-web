cc9976b6f16cc4d1b8ca8dc75d08eef8e9a73c55
corn <dalcon10280@gmail.com>
2024-10-28 01:08:49 +0900

    feat: add error page

--- components/dashboard/Page.vue
+++ components/dashboard/Page.vue
@@ -1,6 +1,16 @@
 <template>
   <div :class="ui.wrapper" v-bind="attrs">
-    <slot />
+    <div v-if="$slots.left" :class="ui.left">
+      <slot name="left" />
+    </div>
+
+    <div :class="centerClass">
+      <slot />
+    </div>
+
+    <div v-if="$slots.right" :class="ui.right">
+      <slot name="right" />
+    </div>
   </div>
 </template>
 
@@ -8,7 +18,14 @@
 import type { PropType } from 'vue'
 
 const config = {
-  wrapper: 'flex flex-1 w-full min-w-0'
+  wrapper: 'flex flex-col lg:grid lg:grid-cols-10 lg:gap-8',
+  left: 'lg:col-span-2',
+  center: {
+    narrow: 'lg:col-span-6',
+    base: 'lg:col-span-8',
+    full: 'lg:col-span-10'
+  },
+  right: 'lg:col-span-2 order-first lg:order-last'
 }
 
 defineOptions({
@@ -26,5 +43,16 @@ const props = defineProps({
   }
 })
 
-const { ui, attrs } = useUI('dashboard.page', toRef(props, 'ui'), config, toRef(props, 'class'), true)
+const slots = useSlots()
+const { ui, attrs } = useUI('page', toRef(props, 'ui'), config, toRef(props, 'class'), true)
+
+const centerClass = computed(() => {
+  if (slots.left && slots.right) {
+    return ui.value.center.narrow
+  } else if (slots.left || slots.right) {
+    return ui.value.center.base
+  }
+
+  return ui.value.center.full
+})
 </script>
---
+++ error.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+import type {PropType} from "vue";
+
+useSeoMeta({
+  title: 'Page not found',
+  description: 'We are sorry but this page could not be found.'
+})
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+
+
+useHead({
+  htmlAttrs: {
+    lang: 'ko'
+  }
+})
+</script>
+
+<template>
+  <div>
+    <DashboardMain>
+      <UContainer>
+        <DashboardPage>
+          <DashboardPageError :error="error" />
+        </DashboardPage>
+      </UContainer>
+    </DashboardMain>
+    <UNotifications />
+  </div>
+</template>
